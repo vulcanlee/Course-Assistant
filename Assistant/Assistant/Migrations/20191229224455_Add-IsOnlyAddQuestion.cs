39243c84dbe5e005d96b83e59d88b6c38f4581af
李進興 Vulcan <vulcan@miniasp.com>
2019-12-30 08:44:25 +0800

    管理者可以看到當時使用者同時連線數量

---
+++ Assistant/Assistant/Migrations/20191229224455_Add-IsOnlyAddQuestion.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assistant.Migrations
+{
+    public partial class AddIsOnlyAddQuestion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnlyAddQuestion",
+                table: "CourseUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnlyAddQuestion",
+                table: "CourseUsers");
+        }
+    }
+}
--- Assistant/Assistant/Services/QuestionBroadcast.cs
+++ Assistant/Assistant/Services/QuestionBroadcast.cs
@@ -8,6 +8,7 @@ namespace Assistant.Services
     public class QuestionBroadcast
     {
         public List<Action<string>> AllNewQuestionEvent { get; set; } = new List<Action<string>>();
+        public List<Action<int>> ConcurrentConnectionEvent { get; set; } = new List<Action<int>>();
         public Task Subscribe(Action<string> action)
         {
             AllNewQuestionEvent.Add(action);
@@ -38,6 +39,33 @@ namespace Assistant.Services
         {
             int all = AllNewQuestionEvent.Count();
             return Task.FromResult(all);
+        }
+        public Task SubscribeConcurrentConnection(Action<int> action)
+        {
+            ConcurrentConnectionEvent.Add(action);
+            return Task.FromResult(0);
+        }
+        public Task UnsubscribeConcurrentConnection(Action<int> action)
+        {
+            var foo = ConcurrentConnectionEvent.FirstOrDefault(x => x == action);
+            if (foo != null)
+            {
+                ConcurrentConnectionEvent.Remove(foo);
+            }
+            return Task.FromResult(0);
+        }
+        public Task PublishConcurrentConnection()
+        {
+            int concurrentConnection = AllNewQuestionEvent.Count();
+            foreach (var item in ConcurrentConnectionEvent)
+            {
+                try
+                {
+                    item?.Invoke(concurrentConnection);
+                }
+                catch { }
+            }
+            return Task.FromResult(0);
         }
     }
 }
